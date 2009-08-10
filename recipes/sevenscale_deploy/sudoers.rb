41e85e7c5549446b03d6fabeb888250a7e4ccf89
Eric Lindvall <eric@5stops.com>
2009-08-10 00:16:26 -0700

    Improving sudoers and users recipes to work with or without sudo.

--- recipes/sevenscale_deploy/sudoers.rb
+++ recipes/sevenscale_deploy/sudoers.rb
@@ -6,102 +6,81 @@
 # Usage:
 #
 # # Enable the user set for the application
-# sudoers_manager.enable fetch(:user)
+# sudoers.enable fetch(:user)
 #
 # # Enable the wheel group
-# sudoers_manager.enable_wheel
-# 
-module SudoersManager
-  @@users = []
+# sudoers.enable_wheel
+#
+Capistrano::Configuration.instance(:must_exist).load do
+  namespace :sudoers do
+    @@users = []
 
-  def users
-    @@users
-  end
+    def users
+      @@users
+    end
 
-  def enable_wheel
-    namespace :sudoers do
-      namespace :apply do
-        desc "Enable users in the wheel group"
-        task :wheel do
-          sudoers_manager.run_as_root do
-            sudoers_manager.apply_to_sudoers [
+    def enable_wheel
+      namespace :sudoers do
+        namespace :apply do
+          desc "Enable users in the wheel group"
+          task :wheel do
+            sudoers.apply_to_sudoers [
               "%wheel	ALL=(ALL)	ALL"
             ]
           end
         end
       end
     end
-  end
 
-  def enable(target_user)
-    @@users << target_user
+    def enable(target_user)
+      @@users << target_user
 
-    namespace :sudoers do
       namespace :apply do
         desc "Apply all sudoers"
         task :default do
-          sudoers_manager.run_as_root do
-            sudoers_manager.users.each do |user|
-              send(user)
-            end
+          sudoers.users.each do |user|
+            send(user)
+          end
 
-            if respond_to?(:wheel)
-              send(:wheel)
-            end
+          if respond_to?(:wheel)
+            send(:wheel)
           end
         end
 
         desc "Apply sudoers for #{target_user}"
         task target_user do
-          sudoers_manager.run_as_root do
-            sudoers_manager.apply_to_sudoers [
-              "Defaults:#{target_user} !requiretty",
-              "#{target_user} ALL=(ALL)       NOPASSWD: ALL"
-            ]
-          end
+          sudoers.apply_to_sudoers [
+            "Defaults:#{target_user} !requiretty",
+            "#{target_user} ALL=(ALL)       NOPASSWD: ALL"
+          ]
         end
       end
     end
-  end
 
-  def apply_to_sudoers(lines)
-    lines = Array(lines)
+    def apply_to_sudoers(lines)
+      via   = fetch(:user) == 'root' ? :run : :sudo
+      lines = Array(lines)
 
-    # Lock sudoers file to prevent visudo from working
-    run %{test '!' -f /etc/sudoers.tmp && touch /etc/sudoers.tmp}
+      # Lock sudoers file to prevent visudo from working
+      invoke_command %{/bin/sh -c "test '!' -f /etc/sudoers.tmp && touch /etc/sudoers.tmp"}, :via => via
 
-    sudoers = capture("cat /etc/sudoers").split(/\r?\n/)
-    sudoers += [ "", lines.reject { |line| sudoers.include?(line) }, "" ]
-      
-    put sudoers.flatten.join("\n"), '/etc/.cap.sudoers', :mode => 0440
+      sudoers = capture("cat /etc/sudoers", :via => via).split(/\r?\n/)
+      sudoers += [ "", lines.reject { |line| sudoers.include?(line) }, "" ]
 
-    run %{visudo -c -f /etc/.cap.sudoers && /usr/bin/install -b -m 440 -o root -g root /etc/.cap.sudoers /etc/sudoers && rm -f /etc/.cap.sudoers /etc/sudoers.tmp }
-  rescue
-    # Remove sudoers lock file
-    run "rm -f /etc/sudoers.tmp"
-    raise
-  end
-  
-  def run_as_root
-    normal_user = fetch(:user)
+      sudoers_temporary = "/tmp/.cap.sudoers.#{$$}"
 
-    if normal_user != 'root'
-      normal_password = fetch(:password)
+      put sudoers.flatten.join("\n"), sudoers_temporary, :mode => 0440
 
-      if fetch(:root_needs_password, true)
-        logger.info "Command must run as root. Please specify root password."
-        set(:user, 'root')
-        set(:password, Capistrano::CLI.password_prompt)
-      end
-    end
-  
-    yield
-  ensure
-    if normal_user != 'root'
-      set(:user, normal_user)
-      set(:password, normal_password)
+      commands = []
+      commands << "/usr/sbin/visudo -c -f #{sudoers_temporary}"
+      commands << "/usr/bin/install -b -m 440 -o root -g root #{sudoers_temporary} /etc/sudoers"
+      commands << "rm -f #{sudoers_temporary} /etc/sudoers.tmp"
+
+      invoke_command %{/bin/sh -c "#{commands.join(' && ')}"}, :via => via
+    rescue
+      # Remove sudoers lock file
+      invoke_command "rm -f #{sudoers_temporary} /etc/sudoers.tmp", :via => via
+      raise
     end
   end
 end
-
-Capistrano.plugin :sudoers_manager, SudoersManager
--- recipes/sevenscale_deploy/users.rb
+++ recipes/sevenscale_deploy/users.rb
@@ -1,12 +1,10 @@
 Capistrano::Configuration.instance(:must_exist).load do
-  namespace :user do
-    @@users = []
-    
+  namespace :users do
     namespace :create do
       desc 'Create all users'
       task :default do
-        tasks.keys.each do |key|
-          send(key) unless key == DEFAULT_TASK
+        tasks.each do |name, task|
+          execute_task(task) unless task == default_task
         end
       end
     end
@@ -19,50 +17,49 @@ Capistrano::Configuration.instance(:must_exist).load do
         end
       end
     end
-  
+
     def create_user(user, options = {})
-      command = "grep -q '^#{user}:' || #{try_sudo} /usr/sbin/useradd '#{user}'"
+      via = fetch(:user) == 'root' ? :run : :sudo
+
+      command = "grep -q '^#{user}:' /etc/passwd || /usr/sbin/useradd '#{user}'"
       args = []
 
       if options[:groups]
         command << " -G #{Array(options[:groups]).join(',')}"
       end
 
-      run command
+      invoke_command %{/bin/sh -c "#{command}"}, :via => via
 
       update_authorized_keys2(user, options[:all_keys])
     end
-    
-    def update_authorized_keys2(user, all_keys = false)
-      key_files = all_keys ? Dir["ssh_keys/*/*"] : Dir["ssh_keys/#{user}/*"]
-      user_keys = key_files.collect { |file| File.read(file).split(/(\r?\n)+/) }.flatten
-    
-      authorized_keys = capture("cat ~#{user}/.ssh/authorized_keys2").split(/\r?\n/)
-    
-      authorized_keys += user_keys.reject { |line| authorized_keys.include?(line) }
-    
-      run("mkdir -p -m 700 ~#{user}/.ssh")
-      put authorized_keys.join("\n"), "~#{user}/.ssh/authorized_keys2", :mode => 0600
-      run("chown -R #{user}.#{user} ~#{user}/.ssh")
-    end
-  end
-  
-  def run_as_root
-    normal_user = fetch(:user)
 
-    if normal_user != 'root'
+    def update_authorized_keys2(user, all_keys = false)
+      via = fetch(:user) == 'root' ? :run : :sudo
+
+      key_files = all_keys ? Dir["ssh_keys/*/*"] : Dir["ssh_keys/#{user}/*"]
+      user_keys = key_files.collect { |file| [ "# #{file}:"] + File.read(file).split(/(\r?\n)+/) }.flatten
+
+      authorized_keys_file = "/tmp/#{user}-authorized_keys2.#{$$}"
+
+      put user_keys.join("\n"), authorized_keys_file, :mode => 0600
+
+      commands = "/usr/bin/install -D -b -m 0600 -o #{user} -g #{user} #{authorized_keys_file} ~#{user}/.ssh/authorized_keys2"
+      commands << " && chown -R #{user}.#{user} ~#{user}/.ssh"
+      commands << " && chmod 0700 ~#{user}/.ssh; rm -f #{authorized_keys_file}"
+
+      invoke_command %{/bin/sh -c "#{commands}"}, :via => via
+    end
+
+    def connect_as_root
+      normal_user = fetch(:user)
       normal_password = fetch(:password)
 
-      if fetch(:root_needs_password, true)
-        logger.info "Command must run as root. Please specify root password."
-        set(:user, 'root')
-        set(:password, Capistrano::CLI.password_prompt)
-      end
-    end
-  
-    yield
-  ensure
-    if normal_user != 'root'
+      logger.info "Command must run as root. Please specify root password."
+      set(:user, 'root')
+      set(:password, Capistrano::CLI.password_prompt)
+
+      yield
+    ensure
       set(:user, normal_user)
       set(:password, normal_password)
     end
