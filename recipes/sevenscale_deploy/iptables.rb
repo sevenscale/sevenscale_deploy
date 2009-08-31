0ed0ad4f06b4929027051066c5926ea132790e74
Eric Lindvall <eric@5stops.com>
2009-08-30 20:32:03 -0700

    Reducing the connections required to setup firewall rules.

--- recipes/sevenscale_deploy/iptables.rb
+++ recipes/sevenscale_deploy/iptables.rb
@@ -6,10 +6,10 @@
 #
 # # Allow web and SSL for app servers
 # iptables.role :app, 80, 443
-# 
+#
 # # Allow ActiveMQ Stomp connections from app instances
 # iptables.role :activemq, 61613, :from_roles => %w(app)
-# 
+#
 # # Allow mysql from any app server
 # iptables.role :db, 3306, :from_roles => %w(app)
 #
@@ -49,11 +49,13 @@ Capistrano::Configuration.instance(:must_exist).load do
             chain_prefix = "#{fetch(:application).upcase}-#{role.to_s.upcase}"
             chain = "#{chain_prefix[0..23]}-INPUT"
 
-            iptables.flush(chain)
+            commands = iptables.flush_commands(chain)
 
-            iptables.rules[role.to_sym].each do |rule|
-              iptables.enable(chain, rule[:port], rule[:protocol], rule)
+            commands += iptables.rules[role.to_sym].collect do |rule|
+              iptables.enable_commands(chain, rule[:port], rule[:protocol], rule)
             end
+
+            sudo %{/bin/sh -c "#{commands.flatten.join(' && ')}"}
 
             iptables.save
           end
@@ -62,25 +64,35 @@ Capistrano::Configuration.instance(:must_exist).load do
     end
 
     def enable(chain, port, protocol, options = {})
+      sudo iptables.enable_commands(chain, port, protocol, options).join(' && ')
+    end
+
+    def flush(chain)
+      sudo %{/bin/sh -c "#{flush_commands(chain).join(' && ')}"}
+    end
+
+    def flush_commands(chain)
+      [
+        # Create a new chain if it doesn't exist or flush it if it does
+        %{(/sbin/iptables -N #{chain} || /sbin/iptables -F #{chain})},
+
+        # If this chain isn't jumped to from INPUT, let's make it so
+        %{(/sbin/iptables -L INPUT | grep -cq #{chain} || /sbin/iptables -I INPUT -j #{chain})}
+      ]
+    end
+
+    def enable_commands(chain, port, protocol, options = {})
       if from_roles = (options[:from_role] || options[:from_roles])
         servers = find_servers(:roles => from_roles, :skip_hostfilter => true).collect do |server|
           IPSocket.getaddress(server.host)
         end
 
-        servers.flatten.uniq.each do |ip_address|
-          sudo %{/sbin/iptables -A #{chain} -p #{protocol} -m #{protocol} -s #{ip_address} --dport #{port} -j ACCEPT}
+        servers.flatten.uniq.collect do |ip_address|
+          %{/sbin/iptables -A #{chain} -p #{protocol} -m #{protocol} -s #{ip_address} --dport #{port} -j ACCEPT}
         end
       else
-        sudo %{/sbin/iptables -A #{chain} -p #{protocol} -m #{protocol} --dport #{port} -j ACCEPT}
+        [ %{/sbin/iptables -A #{chain} -p #{protocol} -m #{protocol} --dport #{port} -j ACCEPT} ]
       end
-    end
-
-    def flush(chain)
-      # Create a new chain if it doesn't exist or flush it if it does
-      sudo %{/bin/sh -c "/sbin/iptables -N #{chain} || /sbin/iptables -F #{chain}"}
-
-      # If this chain isn't jumped to from INPUT, let's make it so
-      sudo %{/bin/sh -c "/sbin/iptables -L INPUT | grep -cq #{chain} || #{sudo} /sbin/iptables -I INPUT -j #{chain}"}
     end
 
     def save
