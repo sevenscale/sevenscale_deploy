cb942dc94dbc11603a7437ee38dcb7f554fc6d95
Eric Lindvall <eric@5stops.com>
2010-01-17 21:06:55 -0800

    Add RAILS_ENV marking.

--- recipes/sevenscale_deploy/rails.rb
+++ recipes/sevenscale_deploy/rails.rb
@@ -1,10 +1,16 @@
 Capistrano::Configuration.instance(:must_exist).load do
   namespace :rails do
+    after 'deploy:finalize_update', 'rails:mark_rails_env'
+
     desc "Install Rails gems with gems:install"
     task :install_gems do
       rails_env = fetch(:rails_env, "production")
 
       run "cd #{latest_release} && #{try_sudo} rake RAILS_ENV=#{rails_env} gems:install"
+    end
+
+    task :mark_rails_env, :except => { :no_release => true } do
+      run "echo #{rails_env} > #{latest_release}/RAILS_ENV"
     end
   end
 end
