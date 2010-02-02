9e876fe81d3d3096c369aff99453931f5bc92472
Eric Lindvall <eric@5stops.com>
2010-02-02 13:48:58 -0800

    Initial release of gem bundler plugin.

---
+++ recipes/sevenscale_deploy/gem_bundler.rb
@@ -0,0 +1,74 @@
+module GemBundler
+  # options:
+  #   :bundle_root
+  #   :shared_subdir
+  #   :gem_root
+  #   :rails
+  #   :cache_cache
+  #   :only
+  #
+  # gem_bundler.bundle :rails => true, :only => rails_env
+  # gem_bundler.bundle :downloader
+  #
+  def bundle(*args)
+    options         = args.is_a?(Hash) ? args.pop : {}
+    outer_namespace = args.first
+    
+    bundle_root_subdir = options[:bundle_root] || outer_namespace
+    
+    if outer_namespace
+      shared_gems_subdir = options[:shared_subdir] || "#{outer_namespace}_gems"
+    else
+      shared_gems_subdir = options[:shared_subdir] || "bundler_gems"
+    end
+    
+    # Default to bundler 0.8
+    if options[:rails]
+      release_gems_subdir = options[:gem_root] || 'vendor/bundler_gems/ruby/1.8'
+    else
+      release_gems_subdir = options[:gem_root] || 'vendor/gems/ruby/1.8'
+    end
+    
+    directories_for_shared = %w(gems specifications)
+    
+    if options[:cache_cache]
+      directories_for_shared << 'cache'
+    end
+    
+    tasks = lambda do
+      namespace :bundler do
+        desc "Symlink the vendored directories to shared"
+        task :symlink_vendor do
+          bundle_root  = File.join(release_path, bundle_root_subdir.to_s)
+          shared_gems  = File.join(shared_path,  shared_gems_subdir)
+          release_gems = File.join(bundle_root,  release_gems_subdir)
+
+          cmd = directories_for_shared.collect do |sub_dir|
+            shared_sub_dir = File.join(shared_gems, sub_dir)
+            "mkdir -p #{shared_sub_dir} && mkdir -p #{release_gems} && ln -s #{shared_sub_dir} #{release_gems}/#{sub_dir}"
+          end.join(' && ')
+
+          run(cmd)
+        end
+
+        desc "Run bundler on a new release"
+        task :bundle_new_release do
+          bundle_root  = File.join(release_path, bundle_root_subdir.to_s)
+          cmd = "cd #{bundle_root} && gem bundle"
+          
+          if options[:only]
+            only = options[:only].respond_to?(:call) ? options[:only].call : options[:only]
+            cmd << " --only #{only}"
+          end
+
+          bundler.symlink_vendor
+          run(cmd)
+        end
+      end
+    end
+    
+    namespace(outer_namespace, &tasks) if outer_namespace
+  end
+end
+
+Capistrano.plugin :gem_bundler, GemBundler
