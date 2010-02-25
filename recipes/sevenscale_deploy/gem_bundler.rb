3c2279a0f5dac9bd8d936289439a7baea6424073
Eric Lindvall <eric@5stops.com>
2010-02-25 11:29:22 -0800

    Update for the bundler 0.9.

--- recipes/sevenscale_deploy/gem_bundler.rb
+++ recipes/sevenscale_deploy/gem_bundler.rb
@@ -16,52 +16,14 @@ module GemBundler
 
     bundle_root_subdir = options[:bundle_root] || outer_namespace
 
-    if outer_namespace
-      shared_gems_subdir = options[:shared_subdir] || "#{outer_namespace}_gems"
-    else
-      shared_gems_subdir = options[:shared_subdir] || "bundler_gems"
-    end
-
-    # Default to bundler 0.8
-    if options[:rails]
-      release_gem_root = options[:gem_root] || 'vendor/bundler_gems'
-    else
-      release_gem_root = options[:gem_root] || 'vendor/gems'
-    end
-
-    release_gem_subdir = options[:gem_subdir]
-
-    directories_for_shared = %w(gems specifications dirs)
-
-    if options[:cache_cache]
-      directories_for_shared << 'cache'
-    end
-
     tasks = lambda do
       namespace :bundler do
-        desc "Symlink the vendored directories to shared"
-        task :symlink_vendor, :roles => options[:roles] do
-          bundle_root  = File.join(release_path, bundle_root_subdir.to_s)
-          shared_gems  = File.join(shared_path,  shared_gems_subdir)
-          release_gems = File.join(bundle_root,  release_gem_root, release_gem_subdir.to_s)
-
-          cmd = directories_for_shared.collect do |sub_dir|
-            shared_sub_dir = File.join(shared_gems, sub_dir)
-            "mkdir -p #{shared_sub_dir} && mkdir -p #{release_gems} && ln -s #{shared_sub_dir} #{release_gems}/#{sub_dir}"
-          end.join(' && ')
-
-          run(cmd)
-        end
-
         desc "Run bundler on a new release"
-        task :bundle_new_release, :roles => options[:roles] do
-          bundle_root  = File.join(release_path, bundle_root_subdir.to_s)
-          release_gems = File.join(bundle_root,  release_gem_root, release_gem_subdir.to_s)
+        task :bundle, :roles => options[:roles] do
+          bundle_root     = File.join(release_path, bundle_root_subdir.to_s)
+          shared_root_dir = File.join(shared_path, 'bundler')
 
-          cmd = "cd #{bundle_root} && bundle install #{release_gems}"
-
-          bundler.symlink_vendor
-          run(cmd)
+          run "cd #{bundle_root} && bundle install #{shared_root_dir}"
         end
       end
     end
@@ -75,7 +37,7 @@ module GemBundler
     namespace :bundler do
       desc 'Install correct version of gem bundler'
       task :install do
-        minimum_version          = '0.9.0'
+        minimum_version          = '0.9.7'
         minumim_rubygems_version = '1.3.5'
 
         commands = [
@@ -90,9 +52,9 @@ module GemBundler
 
     unless options[:hook] == false
       our_hook_name = if outer_namespace
-        "#{outer_namespace}:bundler:bundle_new_release"
+        "#{outer_namespace}:bundler:bundle"
       else
-        "bundler:bundle_new_release"
+        "bundler:bundle"
       end
 
       after_hook = if options[:hook].is_a?(String)
