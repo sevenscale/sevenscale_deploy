2ae9e40bb6421df720ed5f50cefa669a3c25fcf9
Eric Lindvall <eric@5stops.com>
2010-05-11 12:29:01 -0700

    Improving campfire integration.

--- recipes/sevenscale_deploy/campfire.rb
+++ recipes/sevenscale_deploy/campfire.rb
@@ -1,45 +1,54 @@
-# Lifted from:
+# Originally lifted from:
 # http://github.com/vigetlabs/viget_deployment/tree/master/recipes/campfire.rb
-begin
-  require 'uri'
-  require 'tinder'
 
-  namespace :campfire do
-    after "deploy", "campfire:notify"
-    after "deploy:migrations", "campfire:notify"
-    after "deploy:rollback", "campfire:notify"
-    
-    desc '[internal] Announces deployments in one or more Campfire rooms.'
-    task :notify do
-      campfires = fetch(:campfires,nil)
-      notify    = fetch(:campfire_notify,nil)
-      unless campfires.nil? || notify.nil?
-        notify.each do |name|
-          config = campfires[name]
-          campfire = Tinder::Campfire.new(config[:domain], :ssl => config[:ssl])
-          if campfire.login(config[:email], config[:password])
-            if room = campfire.find_room_by_name(config[:room])
-              logger.debug "sending message to #{config[:room]} on #{name.to_s} Campfire"
+namespace :campfire do
+  set(:previous_current_revision) { raise "Previous current revision was never fetched" }
+  
+  before 'deploy',            'campfire:save_previous_current_revision'
+  before 'deploy:migrations', 'campfire:save_previous_current_revision'
+  before 'deploy:rollback',   'campfire:save_previous_current_revision'
+  
+  def register(domain, token, config = {})
+    begin
+      require 'uri'
+      require 'tinder'
+    rescue LoadError
+      return false # skip campfire stuff if tinder can't be required
+    end
 
-              message = "[CAP] %s just deployed revision %s of %s" % [
-                ENV['USER'], current_revision.to_s[0..5], fetch(:application), 
-              ]
-              if stage = fetch(:rails_env, nil)
-                message << " to #{stage}"
-              end
-              room.speak "#{message}."
-              changes = `#{source.local.log(previous_revision, current_revision)}`.chomp
-              room.paste changes unless changes.blank?
-            else
-              logger.debug "Campfire #{name.to_s} room #{config[:room]} not found"
-            end
-          else
-            logger.debug "Campfire #{name.to_s} email and/or password incorrect"
+    short_domain = domain[/^([^\.]+)/, 1]
+
+    namespace short_domain do
+      after 'deploy',            "campfire:#{short_domain}:notify"
+      after 'deploy:migrations', "campfire:#{short_domain}:notify"
+      after 'deploy:rollback',   "campfire:#{short_domain}:notify"
+
+      desc "Notify #{short_domain} of deploy"
+      task :notify do
+        campfire = Tinder::Campfire.new(domain, :ssl => config[:ssl])
+        campfire.login(token, 'x')
+        room = campfire.find_room_by_name(config[:room]) rescue nil
+        
+        if room
+          logger.debug "sending message to #{config[:room]} on #{short_domain} Campfire"
+
+          message = "[CAP] %s just deployed revision %s of %s" % [
+            ENV['USER'], current_revision.to_s[0..5], fetch(:application), 
+          ]
+          if stage = fetch(:rails_env, nil)
+            message << " to #{stage}"
           end
+          room.speak "#{message}."
+          changes = `#{source.local.log(previous_current_revision, current_revision)}`.chomp
+          room.paste changes unless changes.blank?
+        else
+          logger.debug "Campfire #{short_domain} room '#{config[:room]}' not found"
         end
       end
     end
   end
-rescue LoadError
-  nil # skip campfire stuff if tinder can't be required
+  
+  task :save_previous_current_revision do
+    set(:previous_current_revision, capture("cat #{current_path}/REVISION").chomp)
+  end
 end
