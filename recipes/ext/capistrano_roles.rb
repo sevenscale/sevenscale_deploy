b2e023972a587842fbe91c3ece2a7a793930fb49
Eric Lindvall <eric@5stops.com>
2010-06-09 18:33:02 -0700

    Add basic moonshine support.

---
+++ recipes/ext/capistrano_roles.rb
@@ -0,0 +1,25 @@
+::Capistrano::Command.class_eval do
+  def replace_placeholders(command, channel)
+    command.gsub(/\$CAPISTRANO:HOST\$/, channel[:host])
+    command.gsub(/\$CAPISTRANO:ROLES\$/, channel[:server].roles.join(','))
+  end
+end
+
+::Capistrano::ServerDefinition.class_eval do
+  attr_accessor :roles
+end
+
+::Capistrano::Configuration.class_eval do
+  def find_servers_with_role_attr(*args)
+    servers = find_servers_without_role_attr(*args)
+
+    servers.each do |server|
+      server.roles = self.roles.select { |name, server_list| server_list.include?(server) }.map { |name, _| name }
+    end
+
+    servers
+  end
+
+  alias_method :find_servers_without_role_attr, :find_servers
+  alias_method :find_servers, :find_servers_with_role_attr
+end
---
+++ recipes/sevenscale_deploy/moonshine.rb
@@ -0,0 +1,60 @@
+namespace :moonshine do
+  after 'deploy:finalize_update' do
+    apply if fetch(:moonshine_apply, true)
+  end
+
+  desc 'Setup moonshine'
+  task :setup do
+    install_required_gems
+    install_git
+    setup_directories
+  end
+
+  desc 'Install required gems'
+  task :install_required_gems do
+    sudo 'gem install rake --no-rdoc --no-ri'
+    sudo 'gem install puppet -v 0.24.8 --no-rdoc --no-ri'
+    sudo 'gem install shadow_puppet --no-rdoc --no-ri'
+  end
+
+  desc <<-DESC
+  Applies the lib/moonshine_setup_manifest.rb manifest, which replicates the old
+  capistrano deploy:setup behavior.
+  DESC
+  task :setup_directories do
+    moonshine_yml_path            = fetch(:moonshine_yml_path, File.join(ENV['RAILS_ROOT'] || Dir.pwd, 'config', 'moonshine.yml'))
+    moonshine_setup_manifest_path = fetch(:moonshine_setup_manifest, File.expand_path('../../../../moonshine/lib/moonshine_setup_manifest.rb', __FILE__))
+
+    upload moonshine_yml_path.to_s,       '/tmp/moonshine.yml'
+    upload moonshine_setup_manifest_path, '/tmp/moonshine_setup_manifest.rb'
+
+    sudo 'shadow_puppet /tmp/moonshine_setup_manifest.rb; rm /tmp/moonshine_setup_manifest.rb /tmp/moonshine.yml'
+  end
+
+  desc 'Apply the Moonshine manifest for this application'
+  task :apply, :except => { :no_release => true } do
+    moonshine_manifest = fetch(:moonshine_manifest, 'application_manifest')
+
+    sudo "env RAILS_ROOT=#{latest_release} RAILS_ENV=#{fetch(:rails_env)} CAPISTRANO_ROLES=$CAPISTRANO:ROLES$ shadow_puppet #{latest_release}/app/manifests/#{moonshine_manifest}.rb"
+  end
+
+  desc 'Install git'
+  task :install_git do
+    install_git_package
+  end
+
+  def fetch_os_distribution
+    capture("lsb_release -a")[/Distributor ID:\s+(.*)$/, 1].chomp
+  end
+
+  def install_git_package
+    case distribution = fetch_os_distribution
+    when 'Fedora'
+      sudo 'yum install -y git-core'
+    when 'Ubuntu'
+      sudo 'apt-get install -q -y git-core'
+    else
+      raise "Unknown distribution: #{distribution.inspect}"
+    end
+  end
+end
