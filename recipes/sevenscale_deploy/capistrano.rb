139ff503ccf16286927afc348ecc9d636385f6fc
Eric Lindvall <eric@5stops.com>
2010-06-22 13:51:21 -0700

    We don't need the roles config file anymore.

--- recipes/sevenscale_deploy/capistrano.rb
+++ recipes/sevenscale_deploy/capistrano.rb
@@ -1,18 +1,5 @@
 namespace :capistrano do
-  after 'deploy:finalize_update', 'capistrano:write_roles'
   after 'deploy:finalize_update', 'capistrano:write_servers'
-
-  desc 'Write roles config file'
-  task :write_roles do
-    config_hash = {}
-
-    roles.each do |role_name, servers|
-      config_hash[role_name] = servers.collect { |s| s.host }
-    end
-
-    put YAML::dump(config_hash), "#{shared_path}/config/capistrano_roles.yml"
-    run "ln -nsf #{shared_path}/config/capistrano_roles.yml #{release_path}/config/capistrano_roles.yml"
-  end
 
   desc 'Write servers config file'
   task :write_servers do
