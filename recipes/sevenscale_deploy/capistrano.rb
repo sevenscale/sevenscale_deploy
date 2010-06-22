8479965f0738903b28f002d5d1b6e46a09f30634
Eric Lindvall <eric@5stops.com>
2010-06-21 20:53:51 -0700

    Add a yml file that contains the roles and options for servers.

--- recipes/sevenscale_deploy/capistrano.rb
+++ recipes/sevenscale_deploy/capistrano.rb
@@ -1,5 +1,6 @@
 namespace :capistrano do
   after 'deploy:finalize_update', 'capistrano:write_roles'
+  after 'deploy:finalize_update', 'capistrano:write_servers'
 
   desc 'Write roles config file'
   task :write_roles do
@@ -11,5 +12,23 @@ namespace :capistrano do
 
     put YAML::dump(config_hash), "#{shared_path}/config/capistrano_roles.yml"
     run "ln -s #{shared_path}/config/capistrano_roles.yml #{release_path}/config/capistrano_roles.yml"
+  end
+
+  desc 'Write servers config file'
+  task :write_servers do
+    config_hash = {}
+    config_hash['roles'] = {}
+    config_hash['options'] = {}
+
+    roles.each do |role_name, servers|
+      config_hash['roles'][role_name] = servers.collect { |s| s.host }
+
+      servers.each do |server|
+        config_hash['options'][server.host] = server.options
+      end
+    end
+
+    put YAML::dump(config_hash), "#{shared_path}/config/capistrano_servers.yml"
+    run "ln -s #{shared_path}/config/capistrano_servers.yml #{release_path}/config/capistrano_servers.yml"
   end
 end
