41d84a08c649460211e96fa8b59d1bca389e0377
Eric Lindvall <eric@5stops.com>
2010-08-20 13:28:21 -0700

    Allow other filenames to be used for logs:tail.

--- recipes/sevenscale_deploy/logs.rb
+++ recipes/sevenscale_deploy/logs.rb
@@ -3,7 +3,9 @@ depend :remote, :command, 'tail'
 namespace :logs do
   desc "tail log files"
   task :tail do
-    run "tail -f #{shared_path}/log/#{rails_env}.log" do |ch, stream, out|
+    log_filename = fetch(:log_filename, "#{rails_env}.log")
+
+    run "tail -f #{shared_path}/log/#{log_filename}" do |ch, stream, out|
       print out
     end
   end
