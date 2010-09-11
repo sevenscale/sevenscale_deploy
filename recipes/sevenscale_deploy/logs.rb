374210b54eed205d1d3547e578eb0da4aa8742cf
Eric Lindvall <eric@5stops.com>
2010-09-10 17:09:49 -0700

    Don't print a huge stacktrace if we just wanted to exit.

--- recipes/sevenscale_deploy/logs.rb
+++ recipes/sevenscale_deploy/logs.rb
@@ -5,8 +5,12 @@ namespace :logs do
   task :tail do
     log_filename = fetch(:log_filename, "#{rails_env}.log")
 
-    run "tail -f #{shared_path}/log/#{log_filename}" do |ch, stream, out|
-      print out
+    begin
+      run "tail -f #{shared_path}/log/#{log_filename}" do |ch, stream, out|
+        print out
+      end
+    rescue Interrupt
+      exit(0)
     end
   end
 end
