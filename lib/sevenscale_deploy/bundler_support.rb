9a17799b228b4c9390bbab3cbf22f5bc20c30c27
Eric Lindvall <eric@5stops.com>
2010-11-01 16:05:58 -0700

    Add parser for 1.0 Gemfile.lock.

--- lib/sevenscale_deploy/bundler_support.rb
+++ lib/sevenscale_deploy/bundler_support.rb
@@ -3,39 +3,49 @@ module SevenScaleDeploy
     def bundler_dependencies(options = {})
       root = options.delete(:root)
       
-      # Create something to communicate over
-      r, w = IO.pipe
+      parse_gemfile_lock(File.join(root, 'Gemfile.lock')).each do |gem_name|
+        puts "gem_name: #{gem_name}"
+        dependent_recipe = "#{gem_name.gsub(/-/, '_')}_gem_dependencies".to_sym
+        if method_defined?(dependent_recipe)
+          recipe(dependent_recipe, options)
+        end
+      end
+    end
 
-      fork do
-        # Close the read side in the child
-        r.close
-        
-        begin
-          # Make sure we're using the right version of bundler
-          Gem.activate 'bundler', '~> 1.0.3'
-          require 'bundler'
-          definition = Bundler::Definition.build(File.join(root, 'Gemfile'), File.join(root, 'Gemfile.lock'), nil)
-          w.puts YAML::dump(:gems => definition.specs.map { |spec| spec.name })
-        rescue Exception => e
-          w.puts YAML::dump(:error => { :message => e.message, :class => e.class.name })
+    def parse_gemfile_lock(lockfile)
+      return parse_gemfile_lock_0_9(lockfile)
+    rescue ArgumentError
+      return parse_gemfile_lock_1_0(lockfile)
+    end
+
+    def parse_gemfile_lock_1_0(lockfile)
+      gems = []
+      in_gem_list = in_specs_list = false
+      
+      File.read(lockfile).split(/(\r?\n)+/).each do |line|
+        if mode = line[/^(\w+)$/, 1]
+          in_gem_list = mode == 'GEM'
+          in_specs_list = false
+          next
         end
-        w.close
-      end
-      
-      w.close
-      response = YAML::load(r.read)
-      r.close
-      
-      if response[:error]
-        raise "#{response[:error][:class]}: #{response[:error][:message]}"
-      else
-        response[:gems].each do |gem_name|
-          dependent_recipe = "#{gem_name.gsub(/-/, '_')}_gem_dependencies".to_sym
-          if method_defined?(dependent_recipe)
-            recipe(dependent_recipe, options)
-          end
+
+        if !in_specs_list && in_gem_list && line == '  specs:'
+          in_specs_list = true
+          next
+        end
+
+        if in_specs_list && gem_name = line[/^ +(.*?)(?: \(.*)$/, 1]
+          gems << gem_name
         end
       end
+
+      gems.uniq
+    end
+
+    def parse_gemfile_lock_0_9(lockfile)
+      gems = YAML::load_file(lockfile)['specs'].collect do |spec|
+        spec.keys.first
+      end.uniq
     end
   end
 end
