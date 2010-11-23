c1b56feb2376f92acdaf81be6e4a0e110bfce0bc
Troy Davis <troy@sevenscale.com>
2010-11-22 20:32:15 -0800

    working recipe that writes files

--- recipes/sevenscale_deploy/papertrail.rb
+++ recipes/sevenscale_deploy/papertrail.rb
@@ -23,21 +23,10 @@ namespace :papertrail do
   after 'deploy:finalize_update', 'papertrail:write_config_file'
   after 'deploy:finalize_update', 'papertrail:restart'
   
-  def files
-    @@files ||= Hash.new { |h,role| h[role] = [] }
-  end
-
-  def destinations
-    @@destinations ||= Hash.new { |h,role| h[role] = [] }
-  end
-  
-  def active_roles
-    @@files.keys.delete_if { |v| v == :all }
-  end
-
   namespace :apply do
     desc "Write new log_files.yml"
     task :default do
+      puts write_config_file_for(:app)
       parallel do |session|
         papertrail.active_roles.each do |role|
           session.when "in?(:#{role.to_s})", write_config_file_for(role)
@@ -46,45 +35,50 @@ namespace :papertrail do
       end
     end
   end
+
+  desc "Restart remote_syslog"
+  task :restart do
+    sudo "/etc/rc.d/init.d/remote_syslog restart"
+  end
+  
+  def files
+    @@files ||= Hash.new { |h,role| h[role] = [] }
+  end
+
+  def active_roles
+    @@files.keys.delete_if { |v| v == :all }
+  end
+
   
   def all(setting)
     role(:all, setting)
   end
 
-  def role(role, setting)
-    files = []
-    if setting.is_a?(Array)
-      papertrail.files[role.to_sym] += s
-    elsif setting.is_a?(String)
-      papertrail.files[role.to_sym] += [s]
-    elsif setting.is_a(Hash)
-      papertrail.destinations[role.to_sym] = { 
-        'host' => setting['host'],
-        'port' => setting['port']
-      }
+  def destination(host, port = 514)
+    @@dest_host = host
+    @@dest_port = port
+  end
+
+  def role(role, files)
+    if files.is_a?(Array)
+      papertrail.files[role.to_sym] += files
+    elsif files.is_a?(String)
+      papertrail.files[role.to_sym] += [files]
     end
   end
 
   def write_config_file_for(role)
-    files = papertrail.files[:role] || []
+    files = papertrail.files[role] || []
     files += papertrail.files[:all]
     files.uniq!
     
-    destination = papertrail.destinations[:role] || papertrail.destinations[:all]
-
-    return if files.empty? || !destination
+    return if files.empty? || !@@dest_host || !@@dest_port
 
     config_hash = { 'files'       => files,
-                    'destination' => destination }
+                    'destination' => { 'host' => @@dest_host, 'port' => @@dest_port } }
                     
     run "mkdir -p #{shared_path}/config"
     put YAML::dump(config_hash), "#{shared_path}/config/log_files.yml"
     run "ln -nsf #{shared_path}/config/log_files.yml #{release_path}/config/log_files.yml"
-  end
-  
-  desc "Restart remote_syslog"
-  task :restart
-    # needs work
-    sudo %{killall remote_syslog && /usr/bin/remote_syslog -c #{release_path}/config/log_files.yml}
   end
 end
