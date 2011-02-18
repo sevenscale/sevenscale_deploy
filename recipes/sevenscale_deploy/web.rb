ee970873b4bb329aed9ec28cd1537c6e038d6737
Eric Lindvall <eric@5stops.com>
2011-02-17 21:07:53 -0800

    Override deploy:web:enable.

--- recipes/sevenscale_deploy/web.rb
+++ recipes/sevenscale_deploy/web.rb
@@ -1,22 +1,36 @@
 namespace :web do
   desc 'Enable web site'
   task :enable, :roles => :app, :except => { :no_release => true } do
-    run "rm #{current_path}/public/system/maintenance.html"
+    deploy.web.enable
   end
   
-  desc 'Disable web site'
+  desc 'Present a maintenance page to visitors'
   task :disable, :roles => :app, :except => { :no_release => true } do
-    require 'erb'
-    on_rollback { run "rm #{current}/public/system/maintenance.html" }
+    deploy.web.disable
+  end
+end
 
-    reason = ENV['REASON']
-    deadline = ENV['UNTIL']
+namespace :deploy do
+  namespace :web do
+    desc 'Present a maintenance page to visitors'
+    task :disable, :roles => :app, :except => { :no_release => true } do
+      require 'erb'
+      on_rollback { run "rm #{current}/public/system/maintenance.html" }
 
-    maintenance_file = fetch(:maintenance_file, File.join(File.dirname(__FILE__), "assets", "maintenance.rhtml"))
+      reason = ENV['REASON']
+      deadline = ENV['UNTIL']
 
-    template = File.read(maintenance_file)
-    result = ERB.new(template).result(binding)
+      maintenance_file = fetch(:maintenance_file, File.join(File.dirname(__FILE__), "assets", "maintenance.rhtml"))
 
-    put result, "#{current_path}/public/system/maintenance.html", :mode => 0644
+      template = File.read(maintenance_file)
+      result = ERB.new(template).result(binding)
+
+      put result, "#{current_path}/public/system/maintenance.html", :mode => 0644
+    end
+
+    desc 'Enable web site'
+    task :enable, :roles => :app, :except => { :no_release => true } do
+      run "rm #{current_path}/public/system/maintenance.html"
+    end
   end
 end
