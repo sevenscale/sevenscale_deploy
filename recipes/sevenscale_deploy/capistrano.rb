3a442729ddd0dce9847b18b98514e7ebe1b98684
Eric Lindvall <eric@5stops.com>
2011-05-01 22:06:09 -0700

    Oh yeah, use next instead of return.

--- recipes/sevenscale_deploy/capistrano.rb
+++ recipes/sevenscale_deploy/capistrano.rb
@@ -4,7 +4,7 @@ namespace :capistrano do
 
   desc 'Write servers config file'
   task :write_servers do
-    return if fetch(:no_capistrano_servers_yml, false)
+    next if fetch(:no_capistrano_servers_yml, false)
 
     config_hash = {}
     config_hash['roles'] = {}
