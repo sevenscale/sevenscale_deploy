5022d94cef529c4336297d2b48bc3daa0b2ee9da
Eric Lindvall <eric@5stops.com>
2011-05-21 18:52:25 -0700

    Really fix the configure hash merging.

--- lib/sevenscale_deploy/basic_manifest.rb
+++ lib/sevenscale_deploy/basic_manifest.rb
@@ -17,15 +17,7 @@ module SevenScaleDeploy
     end
 
     def self.configure(other_hash)
-      deep_merge = lambda do |h1, h2|
-        h1.to_hash.merge(h2.to_hash) do |key, oldval, newval|
-          oldval = oldval.to_hash if oldval.respond_to?(:to_hash)
-          newval = newval.to_hash if newval.respond_to?(:to_hash)
-          oldval.class.to_s == 'Hash' && newval.class.to_s == 'Hash' ? deep_merge.call(oldval, newval): newval
-        end
-      end
-
-      __config__.replace(deep_merge.call(__config__, other_hash).with_indifferent_access)
+      __config__.replace(__config__.to_hash.deep_merge(other_hash.with_indifferent_access).with_indifferent_access)
     end
 
     # Perform fact-specific overrides
