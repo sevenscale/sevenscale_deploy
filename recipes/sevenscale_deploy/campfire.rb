e79d399bb2d52803991d568dbf50c946c32bbc4f
Eric Lindvall <eric@5stops.com>
2012-05-28 21:52:46 -0700

    Do better campfire notification on deploy

--- recipes/sevenscale_deploy/campfire.rb
+++ recipes/sevenscale_deploy/campfire.rb
@@ -7,10 +7,10 @@
 #
 namespace :campfire do
   set(:previous_current_revision) { raise "Previous current revision was never fetched" }
-  
+
   # before 'deploy',                   'campfire:save_previous_current_revision'
   # before 'deploy:rollback:revision', 'campfire:save_previous_current_revision'
-  
+
   def register(domain, token, config = {})
     begin
       require 'uri'
@@ -21,6 +21,34 @@ namespace :campfire do
 
     short_domain = domain[/^([^\.]+)/, 1]
 
+    speak = lambda do |message|
+      campfire = Tinder::Campfire.new(domain, :ssl => config[:ssl], :token => token)
+      room     = campfire.find_room_by_name(config[:room]) rescue nil
+
+      if room
+        room.speak message
+      end
+    end
+
+    notify_error = lambda do
+      if ex = $!
+        message = "[CAP] %s's deploy of %s" % [
+          ENV['USER'], fetch(:application),
+        ]
+        if stage = fetch(:rails_env, nil)
+          message << " to #{stage}"
+        end
+
+        if ::Interrupt === ex
+          message << " was canceled."
+        else
+          message << " failed with exception #{ex.class}: #{ex.message}"
+        end
+
+        speak.call message
+      end
+    end
+
     namespace short_domain do
       before 'deploy',            "campfire:#{short_domain}:notify_start"
       before 'deploy:migrations', "campfire:#{short_domain}:notify_start"
@@ -28,50 +56,42 @@ namespace :campfire do
       after  'deploy:migrations', "campfire:#{short_domain}:notify_finished"
 
       task :notify_start do
-        campfire = Tinder::Campfire.new(domain, :ssl => config[:ssl], :token => token)
-        room     = campfire.find_room_by_name(config[:room]) rescue nil
+        deployer    = ENV['USER']
+        deployed    = current_revision.to_s[0..7]
+        deploying   = real_revision.to_s[0..7]
+        github_repo = repository[/github.com:(.*)\.git$/, 1]
+        compare_url = "http://github.com/#{github_repo}/compare/#{deployed}...#{deploying}"
 
-        if room
-          deployer    = ENV['USER']
-          deployed    = current_revision.to_s[0..7]
-          deploying   = real_revision.to_s[0..7]
-          github_repo = repository[/github.com:(.*)\.git$/, 1]
-          compare_url = "http://github.com/#{github_repo}/compare/#{deployed}...#{deploying}"
-
-          message = "[CAP] %s is deploying (%s..%s) of %s" % [
-            ENV['USER'], deployed, deploying, fetch(:application),
-          ]
-          if stage = fetch(:rails_env, nil)
-            message << " to #{stage}"
-          end
-
-          message << " with `cap #{ARGV.join(' ')}` (#{compare_url})"
-          room.speak message
+        message = "[CAP] %s is deploying (%s..%s) of %s" % [
+          ENV['USER'], deployed, deploying, fetch(:application),
+        ]
+        if stage = fetch(:rails_env, nil)
+          message << " to #{stage}"
         end
+        message << " with `cap #{ARGV.join(' ')}` (#{compare_url})"
+
+        speak.call message
+
+        # Make sure we say something if there's an error
+        at_exit &notify_error
       end
 
       task :notify_finished do
-        campfire = Tinder::Campfire.new(domain, :ssl => config[:ssl], :token => token)
-        room     = campfire.find_room_by_name(config[:room]) rescue nil
-        
-        if room
-          logger.debug "sending message to #{config[:room]} on #{short_domain} Campfire"
+        logger.debug "sending message to #{config[:room]} on #{short_domain} Campfire"
 
-          message = "[CAP] %s's deploy of %s" % [
-            ENV['USER'], fetch(:application),
-          ]
-          if stage = fetch(:rails_env, nil)
-            message << " to #{stage}"
-          end
-
-          message << " is done."
-
-          room.speak message
+        message = "[CAP] %s's deploy of %s" % [
+          ENV['USER'], fetch(:application),
+        ]
+        if stage = fetch(:rails_env, nil)
+          message << " to #{stage}"
         end
+        message << " is done."
+
+        speak.call message
       end
     end
   end
-  
+
   task :save_previous_current_revision do
     set(:previous_current_revision, (capture("cat #{current_path}/REVISION").chomp rescue nil))
   end
