9ceacc483f1911e4ab97d9cb009e9a1065b115e3
Eric Lindvall <eric@5stops.com>
2013-10-30 22:22:15 -0700

    Update the role for marking

--- recipes/sevenscale_deploy/assets.rb
+++ recipes/sevenscale_deploy/assets.rb
@@ -3,7 +3,7 @@ namespace :assets do
   set(:asset_timestamp) { run_locally(source.local.scm(:log, '-1', '--pretty=format:%cd', '--date=raw', real_revision, '--', *Array(asset_timestamp_directories).flatten))[/^(\d+)/, 1] }
 
   desc 'Mark asset timestamp'
-  task :mark, :roles => :app, :except => { :no_release => true } do
+  task :mark, :roles => :web, :except => { :no_release => true } do
     run "echo #{asset_timestamp} > #{release_path}/ASSET_TIMESTAMP"
   end
 end
